f00ffc75e2d2600f9edd3e47de33fc7388380a05
BuildTools <unconfigured@null.spigotmc.org>
2017-09-20 14:15:27 -0500

    Initial Commit

---
+++ src/me/FlyingHalfMast/main/Main.java
@@ -0,0 +1,15 @@
+package me.FlyingHalfMast.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
